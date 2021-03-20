5cad575f384e0baed615b17c002a98c07ffb0cc1
ElissonLimaVale <elissonllima1@gmail.com>
2021-03-20 17:46:27 -0300

    finalizada página sobre

--- scripts/home.js
+++ scripts/home.js
@@ -1,5 +1,5 @@
 let staticHeight = {'Devs/inicio': 500, 'Devs/artigos': 500, 'Devs/sobre': 580,'Devs/contato': 500, 'Devs/duvidas': 500};
-let heightPhone = {'Devs/inicio': 500, 'Devs/artigos': 500, 'Devs/sobre': 1150,'Devs/contato': 800, 'Devs/duvidas': 500};
+let heightPhone = {'Devs/inicio': 500, 'Devs/artigos': 500, 'Devs/sobre': 950,'Devs/contato': 800, 'Devs/duvidas': 500};
 
 let ActionMenu = {
     "DEV FULL": () => {

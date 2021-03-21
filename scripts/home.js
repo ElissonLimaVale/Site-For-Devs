f27a13926ada53915daeeaefd7e6a1204fe38140
ElissonLimaVale <elissonllima1@gmail.com>
2021-03-21 01:25:09 -0300

    adicionado menu mobile

--- scripts/home.js
+++ scripts/home.js
@@ -1,6 +1,6 @@
 let staticHeight = {'Devs/inicio': 500, 'Devs/artigos': 500, 'Devs/sobre': 580,'Devs/contato': 500, 'Devs/duvidas': 500};
 let heightPhone = {'Devs/inicio': 500, 'Devs/artigos': 500, 'Devs/sobre': 990,'Devs/contato': 800, 'Devs/duvidas': 630};
-
+//aciona o mudanca de página com o click do menu
 let ActionMenu = {
     "DEV FULL": () => {
         document.querySelector("iframe").src = "inicio.html";
@@ -24,13 +24,29 @@ let ActionMenu = {
     }
 };
 
-document.querySelector("#inicio").addEventListener("click", function(e){activeMenu(document.querySelector("#inicio").textContent);});
-document.querySelector("#artigos").addEventListener("click", function(e){activeMenu(document.querySelector("#artigos").textContent);});
-document.querySelector("#sobre").addEventListener("click", function(e){activeMenu(document.querySelector("#sobre").textContent);});
-document.querySelector("#contato").addEventListener("click", function(e){activeMenu(document.querySelector("#contato").textContent);});
-document.querySelector("#duvidas").addEventListener("click", function(e){activeMenu(document.querySelector("#duvidas").textContent);});
+// Métodos de alteração do menu
+let Menu = {
+    "open": () => {
+        document.querySelectorAll(".menu-item").forEach((element) => {
+                element.hidden = false;
+        });
+    },
+    "close": () => {
+        document.querySelectorAll(".menu-item").forEach((element) => {
+            if(element.textContent != "DEV FULL"){
+                element.hidden = true;
+            }
+        });
+    }, 
+    "IsOpen": () => {
+        return document.querySelector("#contato").hidden;
+    }
+}
+document.querySelectorAll(".menu-item").forEach((element) => {
+    element.addEventListener("click", () => {actionMenu(element.textContent);});
+});
 
-function activeMenu(text){
+function actionMenu(text){
     ActionMenu[text]();
 }
 
@@ -38,11 +54,42 @@ function activeMenu(text){
 window.addEventListener("resize", resizing);
 
 function resizing(){
+    let size = window.innerWidth;
     let iframe = document.querySelector("iframe");
     let page = iframe.src.substring(iframe.src.indexOf("Devs/"),iframe.src.indexOf(".html"));
-    if(window.innerWidth > 950){
+    if(size > 950){
         iframe.height = staticHeight[page];
     }else {
         iframe.height = heightPhone[page];
     }
+    resizeMenu(size);
 }
+
+
+///########## foramação e ação do menu #############
+document.querySelector("#icon-menu").addEventListener("click", () => {
+    console.log(Menu["IsOpen"]());
+    if(!Menu["IsOpen"]()){
+        Menu["close"]();
+    }else{
+        Menu["open"]();
+    }
+});
+
+function resizeMenu(size){
+    if(size > 800){
+        Menu['open']();
+    }else{
+        Menu['close']();
+    }
+}
+
+
+
+
+
+
+
+
+///ajusta o menu 
+resizeMenu(window.innerWidth);
